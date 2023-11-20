ecf91108b86dc99267d0d2ac7685ddb3250ec392
JerryLee03 <96854004+JerryLee03@users.noreply.github.com>
2023-11-20 09:54:24 +0800

    Add files via upload

---
+++ test/sql.php
@@ -0,0 +1,9 @@
+<?php
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020004_jerremy michael lee";
+
+    $con = mysqli_connect($server,$user,$password,$database)
+
+?>
